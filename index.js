01d60baa3e15a159e9b42314540e5365d1b2ead5
yashjsalian23 <yashjsalian23@gmail.com>
2020-08-01 23:27:52 +0530

    added CORS settings

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+
+app.use((req, res , next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
 
 const PORT = process.env.PORT || 5000;
 
