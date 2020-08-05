404ca77d72f0cfe1cc5a079420498156922e21ee
yashjsalian23 <yashjsalian23@gmail.com>
2020-08-05 23:07:03 +0530

    removed uri issues

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/", (req,res) => {
 
 const PORT = process.env.PORT || 5000;
 
-// mongoose.connect('mongodb+srv://admin:SyPf9wZFJQwszrgJ@fyp.7qs2e.mongodb.net/test?retryWrites=true&w=majority')
+// mongoose.connect('')
 //   .then(() => {
 //     app.listen(PORT);
 //     console.log("Server Started");
