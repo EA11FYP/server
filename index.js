f114340a4e1a16971dac54f10f2fb88cfe5334a6
yashjsalian23 <yashjsalian23@gmail.com>
2020-08-07 13:50:41 +0530

    fixed local mongo issue

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
- mongoose.connect('mongodb://localhost/surveyApp')
+ mongoose.connect('mongodb://localhost/fyp')
    .then(() => {
      app.listen(PORT);
      console.log("Server Started");
