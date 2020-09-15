c0808f5002f828d509b87b0d1a0f532ed5b556ee
yashjsalian23 <yashjsalian23@gmail.com>
2020-09-15 12:19:59 +0530

    fixed logout error

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -18,8 +18,8 @@ router.post("/mentee/login", authControllers.menteeLogin);
 
 router.post("/mentor/login", authControllers.mentorLogin);
 
-router.post("/mentee/logout", authControllers.logout);
+router.post("/logout", authControllers.logout);
 
-router.post("/mentor/logout", authControllers.logout);
+
 
 module.exports = router;
