9655f11b82943a12289901f8d9a3543922bba6a1
yashjsalian23 <yashjsalian23@gmail.com>
2021-04-24 16:07:08 +0530

    added get req for mentor requests

--- controllers/mentor-controllers.js
+++ controllers/mentor-controllers.js
@@ -1,5 +1,5 @@
 const Mentor = require('../models/mentor');
-const Mentee = require('../models/mentee');
+// const Mentee = require('../models/mentee');
 
 const allMentors = async(req,res,next) => {
     Mentor.find({}, (err,result) => {
@@ -18,4 +18,21 @@ const allMentors = async(req,res,next) => {
     })
 }
 
+const allReq = async(req,res,next) => {
+    Mentor.findById(req.params.id, (err, result) => {
+        if(err){
+            return  res.status(400).send({
+                 success: false,
+                 message: "Cannot find mentor"
+            });
+        } else {
+            return res.status(200).send({
+                success: true,
+                info: result.requests
+            })
+        }
+    }).populate('requests');
+}
+
 exports.allMentors = allMentors;
+exports.allReq = allReq;
--- routes/mentor-routes.js
+++ routes/mentor-routes.js
@@ -5,4 +5,6 @@ const mentorController = require('../controllers/mentor-controllers');
 
 router.get('/', mentorController.allMentors);
 
+router.get('/requests/all/:id', mentorController.allReq);
+
 module.exports = router;
